d34b8f432a9a31b1a81f3af1e293db48712de7f3
Byunghyun Ban <bhban@kakao.com>
2022-03-19 16:07:16 +0900

    .

---
+++ NFT Auto.py
@@ -0,0 +1,15 @@
+"""
+NFT AUTO
+with GUI
+Byunghyun Ban
+https://github.com/needleworm
+"""
+
+import sys
+#from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5 import QtWidgets as Q
+from PyQt5.QtCore import *
+import time
+from ui import Ui_MainWindow
+
